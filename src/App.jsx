ccc76f553d2e18fdd25942be06b41e60d28e8821
hdni2002 <hdni2003@gmail.com>
2025-08-20 20:17:00 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/layout/layout";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout></Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import Header from "../Header/header";
+import * as s from "./styles";
+function Layout({children}) {
+  return (
+    <div css={s.layout}>
+      <Header />
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
